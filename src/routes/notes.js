b49bd635735c7b39904856ff43540987b6b40c96
LordExodia <ks.estupinan@uniandes.edu.co>
2020-04-13 12:08:03 -0500

    utils

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,14 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const { getNotes, createNote, getNote, updateNote, deleteNote } = require('../controllers/notes.controller');
+//const { getNotes, createNote, getNote, updateNote, deleteNote } = require('../controllers/notes.controller');
+const {
+  getNotes,
+  createNote,
+  getNote,
+  updateNote,
+  deleteNote,
+} = require("../db/MongoUtils");
 
-router.route('/')
-    .get(getNotes)
-    .post(createNote)
-router.route('/:id')
-    .get(getNote)
-    .put(updateNote)
-    .delete(deleteNote)
+router.route("/").get(getNotes).post(createNote);
+router.route("/:id").get(getNote).put(updateNote).delete(deleteNote);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const { getUsers, createUser, deleteUser } = require('../controllers/users.controller');
+//const { getUsers, createUser, deleteUser } = require('../controllers/users.controller');
+const { getUsers, createUser, deleteUser } = require("../db/MongoUtils");
 
-router.route('/')
-    .get(getUsers)
-    .post(createUser)
-router.route('/:id')
-    .delete(deleteUser)
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").delete(deleteUser);
 
 module.exports = router;
